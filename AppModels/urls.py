545c47ed8145f6d2be840a06117140948898393d
Jordy Acosta <yorsh248@gmail.com>
2022-01-26 18:52:46 -0300

    aplique herencia y un poco de formulario

--- AppModels/urls.py
+++ AppModels/urls.py
@@ -1,6 +1,6 @@
 from unicodedata import name
 from django.urls import path 
-from AppModels.views import crear_equipo, director_tecnico, equipo, jugador, inicio 
+from AppModels.views import crear_equipo, director_tecnico, equipo, jugador, inicio,equipos_formulario
 
 urlpatterns = [
     path('equipo/<nombre>/<copas_ganadas>', crear_equipo),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('equipo', equipo , name='equipo'),  
     path('jugador', jugador,name='jugador'), 
     path('director_tecnico', director_tecnico,name='director_tecnico'), 
+    path('equiposFormulario',equipos_formulario, name= 'equipos_formulario'),
 ]
--- AppModels/views.py
+++ AppModels/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from AppModels.models import Equipo
 
@@ -12,10 +12,21 @@ def inicio(request):
     return render(request, 'AppModels/inicio.html')
 
 def equipo(request):
-    return render(request, 'AppModels/equipo.html') 
+    return render(request, 'AppModels/equipo.html',
+    {'equipos':Equipo.objects.all()}) 
 
 def jugador(request):
     return render(request, 'AppModels/jugador.html')
 
 def director_tecnico(request):
     return render(request, 'AppModels/director_tecnico.html')
+
+def equipos_formulario(request):
+    if request.method == 'POST':
+        equipo =request.POST['equipo']
+        copas_ganadas=request.POST['copas_ganadas']
+        print(request.POST)
+        Equipo.objects.create(nombre=equipo, copas_ganadas=copas_ganadas)
+        return redirect('equipo')
+    return render (request, 'AppModels/equiposFormulario.html')
+    
