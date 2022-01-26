d83af7eaa2e88b44d2c90fa950e448d2471e2347
Jordy Acosta <yorsh248@gmail.com>
2022-01-25 23:59:13 -0300

    Agregue archivo padre.html

--- AppModels/urls.py
+++ AppModels/urls.py
@@ -1,10 +1,11 @@
+from unicodedata import name
 from django.urls import path 
 from AppModels.views import crear_equipo, director_tecnico, equipo, jugador, inicio 
 
 urlpatterns = [
     path('equipo/<nombre>/<copas_ganadas>', crear_equipo),
-    path('', inicio),
-    path('equipo', equipo),  
-    path('jugador', jugador), 
-    path('director_tecnico', director_tecnico), 
+    path('', inicio, name='inicio'),
+    path('equipo', equipo , name='equipo'),  
+    path('jugador', jugador,name='jugador'), 
+    path('director_tecnico', director_tecnico,name='director_tecnico'), 
 ]
