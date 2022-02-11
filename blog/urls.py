84565443283d4fc5cecee5e147af8bf9e06c86e3
José Robles <jm_12_95@hotmail.com>
2022-02-10 21:40:46 -0300

    Página de login

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from unicodedata import name 
 from django.urls import path 
-from .views import Blogs, Blog, Equipos, Equipo
+from .views import Blogs, Blog, Equipos, Equipo, login_request
 
 urlpatterns = [
     path('', Blogs.as_view() , name='blogs') ,
     path('blog/<pk>', Blog.as_view() , name="blog") ,
     path('equipos/', Equipos.as_view() , name='equipos') ,
     path('equipo/<pk>', Equipo.as_view() , name='equipo') ,
+    path('login/', login_request , name='login') ,
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Blog, Equipo
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
 class Blogs(ListView):
@@ -19,4 +21,30 @@ class Equipo(DetailView):
     model = Equipo
     template_name = 'blog/equipo.html'
     
+def login_request(request):
     
+    if request.method == "POST":
+        form = AuthenticationForm(request, data= request.POST)
+        
+        if form.is_valid():
+            usuario = form.cleaned_data.get('username')
+            clave = form.cleaned_data.get('password')
+            
+            user = authenticate(username=usuario , password=clave)
+            
+            if user is not None:
+                login(request, user)
+                
+                return render(request, 'blog/inicio.html', {'mensaje' : f'Bienvenido {usuario}'})
+            
+            else:
+                
+                return render(request, 'blog/inicio.html', {'mensaje' : 'Los datos ingresados no corresponden a ningún usuario'})
+            
+        else:
+            
+            return render(request, 'blog/inicio.html', {'mensaje' : 'Los datos ingresados no son válidos'})
+
+    form = AuthenticationForm()
+    
+    return render(request, 'blog/login.html' , {'form' : form})
